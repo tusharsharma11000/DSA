81f860b39617fdf2ad2d3069f6c10726ecfc96a0
Tushar Sharma <151189988+tusharsharma11000@users.noreply.github.com>
2026-07-15 20:08:43 +0530

    Time: 0 ms (100.00%) | Memory: 10.5 MB (51.53%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
@@ -5,15 +5,13 @@ public:
         stack<string> st;
         string word;
         stringstream ss(s);
-
-        // Store every word in the stack
+        
         while (ss >> word) {
             st.push(word);
         }
 
         string ans = "";
 
-        // Pop words one by one
         while (!st.empty()) {
             ans += st.top();
             st.pop();
