a12acc5bc8b555605aab3021a07d64e104c4ca53
Tushar Sharma <151189988+tusharsharma11000@users.noreply.github.com>
2026-07-21 12:10:07 +0530

    Time: 9 ms (59.21%) | Memory: 26.5 MB (83.30%) - LeetSync

---
+++ 646-maximum-length-of-pair-chain/maximum-length-of-pair-chain.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    static bool cmp(vector<int>& a, vector<int>& b) {
+        return a[1] < b[1];
+    }
+
+    int findLongestChain(vector<vector<int>>& pairs) {
+
+        sort(pairs.begin(), pairs.end(), cmp);
+
+        int count = 1;
+        int end = pairs[0][1];
+
+        for (int i = 1; i < pairs.size(); i++) {
+
+            if (pairs[i][0] > end) {
+                count++;
+                end = pairs[i][1];
+            }
+        }
+
+        return count;
+    }
+};
