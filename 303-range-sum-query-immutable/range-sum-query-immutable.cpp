9470212b460389b2aab1152d84548aebf02ea69f
Tushar Sharma <151189988+tusharsharma11000@users.noreply.github.com>
2026-07-24 11:37:15 +0530

    Time: 0 ms (100.00%) | Memory: 23.9 MB (62.97%) - LeetSync

--- 303-range-sum-query-immutable/range-sum-query-immutable.cpp
+++ 303-range-sum-query-immutable/range-sum-query-immutable.cpp
@@ -1,19 +1,22 @@
-
- class NumArray {
+class NumArray {
 public:
-    vector<int> nums;
 
-    NumArray(vector<int>& arr) {
-        nums = arr;
+    vector<int> prefix;
+
+    NumArray(vector<int>& nums) {
+
+        prefix.resize(nums.size());
+        prefix[0] = nums[0];
+
+        for (int i = 1; i < nums.size(); i++)
+            prefix[i] = prefix[i-1] + nums[i];
     }
 
     int sumRange(int left, int right) {
 
-        int sum = 0;
+        if (left == 0)
+            return prefix[right];
 
-        for (int i = left; i <= right; i++)
-            sum += nums[i];
-
-        return sum;
+        return prefix[right] - prefix[left-1];
     }
 };
